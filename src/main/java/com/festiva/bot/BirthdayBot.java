3545aaab6f020e55587d6b29272553ce42d74dd2
zufar-sunagatov <zufar.sunagatov@gmail.com>
2025-04-14 00:15:28 +0100

    BirthdayBot class constructor was updated

--- src/main/java/com/festiva/bot/BirthdayBot.java
+++ src/main/java/com/festiva/bot/BirthdayBot.java
@@ -1,7 +1,6 @@
 package com.festiva.bot;
 
 import com.festiva.command.CommandRouter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,17 +11,21 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class BirthdayBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
 
     private final CommandRouter commandRouter;
     private final Callback callbackQuery;
 
-    @Value("${telegram.bot.token}")
-    private String botToken;
-
-    @Value("${telegram.bot.username}")
-    private String botUsername;
+    public BirthdayBot(CommandRouter commandRouter,
+                       Callback callbackQuery,
+                       @Value("${telegram.bot.token}") String botToken) {
+        super(botToken);
+        this.commandRouter = commandRouter;
+        this.callbackQuery = callbackQuery;
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -46,7 +49,6 @@ public class BirthdayBot extends TelegramLongPollingBot {
 
     /**
      * Processes a callback query update.
-     *
      */
     private void processCallbackQuery(Update update) throws Exception {
         EditMessageText editMessage = callbackQuery.handleCallbackQuery(update.getCallbackQuery());
@@ -60,7 +62,6 @@ public class BirthdayBot extends TelegramLongPollingBot {
 
     /**
      * Processes a message update by routing the update and sending the result.
-     *
      */
     private void processMessageUpdate(Update update) throws Exception {
         SendMessage response = commandRouter.route(update);
@@ -77,14 +78,8 @@ public class BirthdayBot extends TelegramLongPollingBot {
         return botUsername;
     }
 
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
     /**
      * Convenience method to send a message externally.
-     *
      */
     public void sendMessage(SendMessage message) {
         try {
